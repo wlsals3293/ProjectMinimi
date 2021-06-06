a5a9ecea22b998317479cea3b50b9ef61ed0eb36
Grara <nomj18@gmail.com>
2021-06-06 23:20:52 +0900

    기믹 임시 배치 및 작동이 유지되는 스위치 추가

--- Assets/Scripts/Gimmick_temp/Switchs_Ctrl.cs
+++ Assets/Scripts/Gimmick_temp/Switchs_Ctrl.cs
@@ -4,72 +4,69 @@ using UnityEngine;
 
 public class Switchs_Ctrl : MonoBehaviour
 {
+    public enum SwitchType {Maintain, OnOff}
+    public SwitchType _type = SwitchType.OnOff;
+
+    [HideInInspector]
     public Switch_C_OBJ _connetObj;
+
+    [HideInInspector]
     public bool isActivate = false;
-    public int collisionCount = 0;
-    public Material[] _color;
-    public Renderer _thisColor;
-    public LayerMask layers;
+    
+    public Material[] _color_DontTouch;
+    Renderer _thisColor;
+    LayerMask layers;
 
     // Start is called before the first frame update
     void Start()
     {
         _thisColor = GetComponent<Renderer>();
         layers = LayerMask.GetMask("Player", "Object", "Minimi");
+        
     }
 
     public void Connecting(Switch_C_OBJ connectingObj)
     {
         _connetObj = connectingObj;
     }
-
+    
+    
     // Update is called once per frame
     void Update()
     {
-        RaycastHit hit;
-        isActivate = Physics.BoxCast(
-            new Vector3(transform.position.x, transform.position.y - 1f, transform.position.z),
-            transform.lossyScale / 2,Vector3.up, 
-            out hit, transform.rotation, 1.2f, 
-            layers, QueryTriggerInteraction.Ignore);
+        
+
+        if ((_type == SwitchType.Maintain && !isActivate) || _type == SwitchType.OnOff)
+            {
+
+            RaycastHit hit;
+
+            isActivate = Physics.BoxCast(
+                new Vector3(transform.position.x, transform.position.y - (transform.lossyScale.y / 2), transform.position.z),
+                transform.lossyScale / 2, 
+                Vector3.up,
+                out hit, 
+                transform.rotation,
+                (transform.lossyScale.y/2) + 0.5f,
+                layers, 
+                QueryTriggerInteraction.Ignore);
+            
+            }
+
+        
+
 
         if (isActivate)
         {
             
-            _thisColor.material = _color[0];
+            _thisColor.material = _color_DontTouch[0];
         }
         if (!isActivate)
         {
-            _thisColor.material = _color[1];
+            _thisColor.material = _color_DontTouch[1];
         }
 
         _connetObj.SwitchCheck();
     }
 
-    
-    /*void OnCollisionEnter(Collision other)
-    {
-        if (other.gameObject.tag == "Player" || other.gameObject.tag == "Respawn")
-        {
-            collisionCount++;
-            isActivate = true;
-            _connetDoor.SwichCheck();
-            _thisColor.material = _color[0];
-        }
-    }
-
-    private void OnCollisionExit(Collision other)
-    {
-        if(other.gameObject.tag == "Player" || other.gameObject.tag == "Respawn")
-        {
-            collisionCount--;
-            if(collisionCount == 0)
-            {
-                isActivate = false;
-                _thisColor.material = _color[1];
-            }
-
-        }
-    }
-    */
 }
